8acea64462e872d48e26ec7931ddbf4b4186e3f7
Pedro Ernesto Perdoná Rech <p.perdona@softplan.com.br>
2020-07-01 14:25:42 -0300

    add repository by typeorm

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,24 +1,28 @@
-import { parseISO, startOfHour } from 'date-fns';
+import { parseISO } from 'date-fns';
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentsService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  return response.json(appointmentsRepository.all());
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentsService(appointmentsRepository);
+    const createAppointmentService = new CreateAppointmentsService();
 
-    const appointment = createAppointmentService.execute({ date: parsedDate, provider });
+    const appointment = await createAppointmentService.execute({ date: parsedDate, provider });
 
     return response.json(appointment);
   } catch (error) {
