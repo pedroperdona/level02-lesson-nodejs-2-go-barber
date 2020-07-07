bdc7174937f8668bae5ace022fe9a11389b68a42
Pedro Ernesto Perdoná Rech <p.perdona@softplan.com.br>
2020-07-06 22:09:55 -0300

    add patch for avatar and multer config

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import ensureAuthenticade from '../middlewares/ensureAuthenticade';
 import CreateUserService from '../services/CreateUserService';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -17,6 +21,10 @@ usersRouter.post('/', async (request, response) => {
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticade, upload.single('avatar'), async (request, response) => {
+  return response.json({ ok: true });
 });
 
 export default usersRouter;
